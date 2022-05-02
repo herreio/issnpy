2dbef69e8fdadce46e30985251851c8eba24a43c
Donatus Herre <donatus.herre@slub-dresden.de>
2022-05-02 13:53:30 +0200

    Parse URL of record

--- issnpy/record.py
+++ issnpy/record.py
@@ -4,14 +4,12 @@ def _get_field(data, name):
             return data[name]
 
 
-def _get_title(data, issn):
-    response_graph = _get_graph(data)
-    response_graph_data = _get_issn_fields(response_graph, issn)
-    if response_graph_data and "mainTitle" in response_graph_data:
-        main_title = response_graph_data["mainTitle"]
-        main_title = main_title.strip(".")
-        main_title = main_title.strip()
-        return main_title
+def _get_graph(data):
+    return _get_field(data, "@graph")
+
+
+def _get_context(data):
+    return _get_field(data, "@context")
 
 
 def _get_issn_l(data, issn):
@@ -21,14 +19,6 @@ def _get_issn_l(data, issn):
         nid = _get_field(node, "@id")
         if pattern in nid:
             return nid.replace(pattern, "")
-
-
-def _get_issn_fields(data, issn):
-    pattern = "resource/ISSN/{0}".format(issn)
-    for d in data:
-        did = _get_field(d, "@id")
-        if did == pattern:
-            return d
 
 
 def _get_issn_key_title(data, issn):
@@ -41,12 +31,29 @@ def _get_issn_key_title(data, issn):
                 return node["value"]
 
 
-def _get_graph(data):
-    return _get_field(data, "@graph")
+def _get_issn_fields(data, issn):
+    pattern = "resource/ISSN/{0}".format(issn)
+    for d in data:
+        did = _get_field(d, "@id")
+        if did == pattern:
+            return d
 
 
-def _get_context(data):
-    return _get_field(data, "@context")
+def _get_title(data, issn):
+    response_graph = _get_graph(data)
+    response_graph_data = _get_issn_fields(response_graph, issn)
+    if response_graph_data and "mainTitle" in response_graph_data:
+        main_title = response_graph_data["mainTitle"]
+        main_title = main_title.strip(".")
+        main_title = main_title.strip()
+        return main_title
+
+
+def _get_url(data, issn):
+    response_graph = _get_graph(data)
+    response_graph_data = _get_issn_fields(response_graph, issn)
+    if response_graph_data and "url" in response_graph_data:
+        return response_graph_data["url"]
 
 
 class Parser:
@@ -69,3 +76,6 @@ class Parser:
 
     def get_issn_l(self):
         return _get_issn_l(self.raw, self.id)
+
+    def get_url(self):
+        return _get_url(self.raw, self.id)

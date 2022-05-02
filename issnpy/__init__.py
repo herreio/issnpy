c8375838a73be97e33986537d2d2b291eeb25863
Donatus Herre <donatus.herre@slub-dresden.de>
2022-05-02 13:55:29 +0200

    Set version number

--- issnpy/__init__.py
+++ issnpy/__init__.py
@@ -4,6 +4,6 @@ This package allows to access linked open data from the ISSN Portal.
 """
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "0.1.0"
+__version__ = "0.0.0"
 
 from .client import request as fetch

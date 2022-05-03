278ac94ae101502dbf78cb551f0218cd4e8b7477
Donatus Herre <donatus.herre@slub-dresden.de>
2022-05-03 19:53:34 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="issnpy",
-    version="0.1.1",
+    version="0.1.2",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="ISSN Portal LD Client",
